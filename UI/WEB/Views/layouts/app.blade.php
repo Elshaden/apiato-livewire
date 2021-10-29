9990816330efc73ad6e853f55d20f173b4dd9cd1
Salah <salah@elshaden.com>
2021-10-29 09:27:25 +0100

    Removed Layout divs, Fixed App Name

--- UI/WEB/Views/layouts/app.blade.php
+++ UI/WEB/Views/layouts/app.blade.php
@@ -5,21 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Livewire</title>
+    <title>{{config('app.name')}}</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
     <!-- Styles -->
-@livewireStyles
+    @livewireStyles
 </head>
-<body lass="font-body antialiased ">
-   <div class="min-h-screen bg-blue-800 " >
+
+<body >
+
+
 @yield('content')
 
-   </div>
+
 @livewire('livewire-ui-modal')
 @livewireUIScripts
 @livewireScripts
