d9868bee9ea9c3b8cf6acac7ea1b5170d18e0f82
Salah <salah@elshaden.com>
2021-11-30 11:04:49 +0100

    Version 3 Changing the Location of Livewire to Its Own Section

--- Bootstrap/livewire-components.php
+++ Bootstrap/livewire-components.php
@@ -1,4 +1,3 @@
 <?php return array (
-  'customer.main' => 'App\\Containers\\AppSection\\Livewire\\Http\\Customer\\Main',
-  'customer.modal' => 'App\\Containers\\AppSection\\Livewire\\Http\\Customer\\Modal',
+
 );
